03a50aa923a68d8d2b5ef0acef2ef3c9098555cd
benearnthof <benearnthof@hotmail.de>
2019-11-03 12:22:21 +0200

    add comments to datasets

--- datenimport.R
+++ datenimport.R
@@ -1,4 +1,8 @@
 # importing data sets 
-crowd <- read.csv("~/statprakt/crowd.csv", encoding="UTF-8")
-summary(crowd)
-inschriften <- read.csv("~/statprakt/inschriften.csv", encoding="UTF-8")
+# eintragungen ins crowdsourcing tool
+crowd <- read.csv("~/statprakt/crowd.csv", encoding = "UTF-8")
+# daten zu lateinischen inschriften
+inschriften <- read.csv("~/statprakt/inschriften.csv", encoding = "UTF-8")
+# publicity aktionen fuer das crowdsourcing tool
+publicity <- read.csv("~/statprakt/publicity.csv", encoding = "UTF-8")
+
