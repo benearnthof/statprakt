ac9b3ba06abe3b8b443512c60a54b5fa4288ae33
benearnthof <benearnthof@hotmail.de>
2019-11-03 14:17:40 +0200

    add first maps

---
+++ inschriftenmap.R
@@ -0,0 +1,43 @@
+# analysis of inscriptions
+# https://geocompr.robinlovelace.net/adv-map.html
+# https://trucvietle.me/r/tutorial/2017/01/18/spatial-heat-map-plotting-using-r.html
+inschriften <- read.csv("~/statprakt/inschriften.csv", encoding = "UTF-8")
+require(sf)
+require(raster)
+require(sp)
+require(mapview)
+coords <- inschriften$Geodaten
+coords <- as.character(coords)
+
+test <- sub("POINT", "", coords)
+test <- sub("\\(", "", test)
+test <- sub("\\)", "", test)
+
+list <- stringr::str_split(test, " ")
+one <- sapply(list, `[[`, 1)
+two <- sapply(list, `[[`, 2)
+
+lat <- two
+lng <- one
+
+points <- data.frame(lat = as.numeric(lat), lng = as.numeric(lng))
+coordinates(points) <- ~lng+lat
+crs(points) <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0" 
+
+require(ggmap)
+map <- get_map(location=c(lon=-75.1803458, lat=39.98352197), zoom=11, maptype='roadmap', color='bw')
+
+counts <- as.data.frame(table(coords))
+test <- sub("POINT\\(", "", counts$coords)
+test <- sub("\\)", "", test)
+list <- stringr::str_split(test, " ")
+# add the coords to the counts data frame
+counts$lat <- sapply(list, `[[`, 2)
+counts$lng <- sapply(list, `[[`, 1)
+counts$lat <- as.numeric(counts$lat)
+counts$lng <- as.numeric(counts$lng)
+
+coordinates(counts) <- ~lng+lat
+crs(counts) <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0" 
+mapview(counts)
+
