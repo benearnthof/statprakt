186efb2a214b9740d4d34b45232fe7e9136996fc
Your Name <julia.hoepler@gmail.com>
2019-11-10 14:14:51 +0100

    Aktualisierung von crowd01_mean_geo

--- crowd01_mean_geo.R
+++ crowd01_mean_geo.R
@@ -20,8 +20,34 @@ Breite_crowd01 <- as.numeric(lat2)
 
 Länge_crowd01 <- as.numeric(lng2)
 
+## Kenngrößen 
 mean(Breite_crowd01)
 #47.18558
 mean(Länge_crowd01)
 #11.11622
 #Mittelpunkt(11.11622 47.18558)
+
+##Streuung
+var(Breite_crowd01)
+#0.970931
+var(Länge_crowd01)
+#3.41473
+
+sd(Breite_crowd01)
+#0.9853583
+sd(Länge_crowd01)
+#1.847899
+
+range(Breite_crowd01)
+#37.67845 50.38567
+range(Länge_crowd01)
+#5.397995 16.268321
+
+## Kenngrößen Zusammenfassung
+summary(Länge_crowd01)
+# Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+#5.398   9.870  11.618  11.116  12.313  16.268 
+
+summary(Breite_crowd01)
+# Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+#37.68   46.47   47.15   47.19   47.84   50.39 
