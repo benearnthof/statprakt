fd1482a8248a49d0261fa38a961206900aeaf6f2
Your Name <julia.hoepler@gmail.com>
2019-11-16 12:08:48 +0100

    Barplot

--- Haupkategorie_crowd01.R
+++ Haupkategorie_crowd01.R
@@ -7,11 +7,7 @@ Hauptkategorie2 <- unique(crowd01$Hauptkategorie)
 #[10] Zeit                   Landschaftsformationen Wetter                
 #12 Levels: Ackerbau Allgemein Backen Fauna Flora Holzverarbeitung ... Zeit
 
-#color_code <- c("Milchverabeitung" = "red", "Viehhaltung" = "blue", "Holzverarbeitung" = "green", "Allgemein" = "yellow", "Backen" = "orange",  )
-#ggplot(crowd01, aes(x = Hauptkategorie)) + geom_boxplot()  +
- # theme(axis.text.x = element_text(angle = 90))
-
-A <- table(crowd01$Hauptkategorie)
+table(crowd01$Hauptkategorie)
 #Ackerbau              Allgemein                 Backen 
 #389                   2047                    412 
 #Fauna                  Flora       Holzverarbeitung 
@@ -21,12 +17,9 @@ A <- table(crowd01$Hauptkategorie)
 #Viehhaltung                 Wetter                   Zeit 
 #3724                    356                    129 
 
-plot(sort(A, decreasing = TRUE), ylab = "absolute Häufigkeit", 
-     las = 2)
-
- <-sort(A, decreasing = TRUE)
 
 ggplot(crowd01, aes(x = reorder(Hauptkategorie, Hauptkategorie,
-                                function(x)-length(x)))) + geom_bar() +theme(axis.text.x = element_text(angle = 90)) + labs(x = "")
+                                function(x)-length(x)))) + geom_bar() +
+  theme(axis.text.x = element_text(angle = 90)) + labs(x = "")
  
 
