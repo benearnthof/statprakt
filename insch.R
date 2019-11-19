72822d8080adf9d1a64002a87cf83c3f6b397252
Arnthof <B.Arnthof@campus.lmu.de>
2019-11-19 15:08:25 +0100

    add rest of insch.R

--- insch.R
+++ insch.R
@@ -1,3 +1,31 @@
 # inschriften: step 1: Betrachten der Daten
 tab <- read.csv("~/statprakt/tabula.csv", encoding="UTF-8")
-heaD(tab)
+head(tab)
+
+x <- tab$Geodaten
+dta <- x[-length(x)]
+
+get_coords <- function(x) {
+  tmp <- as.character(x)
+  tmp <- sub("POINT", "", tmp)
+  tmp <- sub("\\(", "", tmp)
+  tmp <- sub("\\)", "", tmp)
+  list <- stringr::str_split(tmp, " ")
+  lng <- as.numeric(sapply(list, `[[`, 1))
+  lat <- as.numeric(sapply(list, `[[`, 2))
+  list(lng = lng, lat = lat)
+}
+
+coords <- get_coords(dta)
+
+length(coords[[1]])
+
+crds <- cbind(coords[[1]], coords[[2]])
+crds <- as.data.frame.matrix(crds)
+names(crds) <- c("lng", "lat")
+
+coordinates(crds) <-  ~ lng + lat
+plot(crds)
+
+inschriften <- read.csv("~/statprakt/inschriften.csv", encoding = "UTF-8")
+head(inschriften)
