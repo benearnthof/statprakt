53dbdfcebcaf829f33c18894d1281076360e1d85
benearnthof <benearnthof@hotmail.de>
2019-11-23 12:28:17 +0200

    add latvor points to map

--- insch.R
+++ insch.R
@@ -1,4 +1,6 @@
 # inschriften: step 1: Betrachten der Daten
+require(raster)
+require(sf)
 tab <- read.csv("~/statprakt/tabula.csv", encoding="UTF-8")
 head(tab)
 
@@ -36,7 +38,31 @@ insch_crds <- data.frame(lng = insch_coords[[1]], lat = insch_coords[[2]])
 names(insch_crds) <- c("lng", "lat")
 head(insch_crds)
 coordinates(insch_crds) <- ~ lng + lat
+plot(insch_crds) # dauert sehr lang => einzigartige koordinatenpaare rausfiltern
+insch_crds@coords <- unique(insch_crds@coords)
 plot(insch_crds)
-plot(crds, col = "red", add = TRUE)
+plot(crds, col = "blue", add = TRUE)
 
 head(inschriften)
+
+# Frage: Gibt es eine Häufung romanischer Basistypen in der Nähe von Zentren 
+# lateinischer Inschriftenfunde?
+
+fulldta <- read.csv("~/statprakt/z_ling.csv", encoding="UTF-8")
+# kommentar online: Distanzmaß für ähnlichkeit der Begriffe zueinander & Ranking
+summary(fulldta$Base_Type_Lang)
+
+# nur eintraege aus lateinischem und vorroemischem typ
+latvor <- fulldta[fulldta$Base_Type_Lang %in% c("lat", "vor"),]
+
+head(latvor$Geo_Data)
+
+latvor_coords <- get_coords(latvor$Geo_Data)
+latvor_coords <- cbind(latvor_coords[[1]], latvor_coords[[2]])
+latvor_coords <- as.data.frame.matrix(latvor_coords)
+latvor_coords <- unique(latvor_coords)
+names(latvor_coords) <- c("lng", "lat")
+
+coordinates(latvor_coords) <-  ~ lng + lat
+plot(latvor_coords, add = T, col = "red")
+
