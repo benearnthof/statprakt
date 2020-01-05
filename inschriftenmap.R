da4cf2091464c4c5aeb16e3403d0a8d096b1e23d
benearnthof <benearnthof@hotmail.de>
2020-01-05 14:20:20 +0100

    clean up inschriftenmap.R

--- inschriftenmap.R
+++ inschriftenmap.R
@@ -1,8 +1,8 @@
-# analysis of inscriptions
+# analyse von inschriften
 # https://geocompr.robinlovelace.net/adv-map.html
 # https://trucvietle.me/r/tutorial/2017/01/18/spatial-heat-map-plotting-using-r.html
 # https://www.r-spatial.org/r/2018/10/25/ggplot2-sf.html
-inschriften <- read.csv("~/statprakt/inschriften.csv", encoding = "UTF-8")
+inschriften <- read.csv("inschriften.csv", encoding = "UTF-8")
 require(sf)
 require(raster)
 require(sp)
@@ -21,7 +21,7 @@ two <- sapply(list, `[[`, 2)
 lat <- two
 lng <- one
 
-#in Datensatz Inschriften Spalte mit Breite und Länge hinzufügen
+#in Datensatz Inschriften Spalte mit Breite und Laenge hinzufügen
 inschriften$Laenge <- lng
 inschriften$Breite <- lat
 
@@ -35,7 +35,7 @@ counts <- as.data.frame(table(coords))
 test <- sub("POINT\\(", "", counts$coords)
 test <- sub("\\)", "", test)
 list <- stringr::str_split(test, " ")
-# add the coords to the counts data frame
+# fuege koordinaten dem datensatz hinzu
 counts$lat <- sapply(list, `[[`, 2)
 counts$lng <- sapply(list, `[[`, 1)
 counts$lat <- as.numeric(counts$lat)
@@ -47,9 +47,49 @@ mapview(counts)
 
 require(ggmap)
 
+# erzeugen des canvas
+sp_crowd <- readRDS(file = "sp_crowd.RDS")
+box <- sp_crowd@bbox
+box[2,] <- c(44, 49)
+box[,1] <- c(4.5, 43.3)
+box[,2] <- c(16.5, 48.5)
+map <- get_stamenmap(bbox = box, zoom = 6, maptype = "toner-lite") 
+canvas <- ggmap(map)
 
-canvas + geom_point(data = as.data.frame.matrix(points@coords), 
-                    aes(x = lng, y = lat), alpha = 0.5) +
+get_coords <- function(x) {
+  tmp <- as.character(x)
+  tmp <- sub("POINT", "", tmp)
+  tmp <- sub("\\(", "", tmp)
+  tmp <- sub("\\)", "", tmp)
+  list <- stringr::str_split(tmp, " ")
+  lng <- as.numeric(sapply(list, `[[`, 1))
+  lat <- as.numeric(sapply(list, `[[`, 2))
+  list(lng = lng, lat = lat)
+}
+
+# datenimport fuer tabula
+tabula <- read.csv("tabula.csv", encoding = "UTF-8", sep = ",")
+tabula <- tabula[-nrow(tabula)]
+x <- tabula$Geodaten
+dta <- x#[-length(x)]
+coords_tabula <- get_coords(dta)
+# tabula <- tabula[-(490), ]
+tabula$lat = coords_tabula$lat
+tabula$lng = coords_tabula$lng
+coordinates(tabula) <- ~ lng + lat
+
+# erzeugen der karten: 
+# einmal tabula mit inschriften 
+# einmal nur inschriften 
+tabula_inschriften <- canvas + geom_point(data = as.data.frame.matrix(points@coords), 
+                                          aes(x = lng, y = lat), alpha = 0.5) +
   geom_point(data = as.data.frame.matrix(tabula@coords),
              aes(x = lng, y = lat), col = "red", size = 2)
 
+ggsave("tabula_inschriften_map.png", plot = tabula_inschriften, 
+       width = 18, height = 12, units = "cm")
+
+inschriften <- tabula_inschriften <- canvas + geom_point(data = as.data.frame.matrix(points@coords), 
+                                                         aes(x = lng, y = lat), alpha = 0.5)
+ggsave("inschriften_ohne_tabula.png", plot = inschriften,
+       width = 18, height = 12, units = "cm")
